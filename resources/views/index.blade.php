3acb3e775261ac6c68a99e50f3a312916cae7357
Victor Figueroa <5786237+oceanskyweb@users.noreply.github.com>
2025-06-19 08:02:56 -0400

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'The list of tasks')
+
+@section('content')
+    {{-- @if (count($tasks)) --}}
+    @forelse ($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+        </div>
+    @empty
+        <div>There are no tasks!</div>
+    @endforelse
+    {{-- @else --}}
+    {{-- <div>There are no tasks!</div> --}}
+    {{-- @endif --}}
+@endsection
